c1d33daa525a77cd1fe6a96ffb3e702fe10cf302
Daniel <casas0375@gmail.com>
2024-09-03 17:52:20 -0600

    V 1.0.0

--- src/app/pages/activities/activities.component.ts
+++ src/app/pages/activities/activities.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ContentfulService } from 'src/app/services/contentful.service';
 
 @Component({
   selector: 'app-activities',
   templateUrl: './activities.component.html',
   styleUrls: ['./activities.component.css']
 })
-export class ActivitiesComponent {
+export class ActivitiesComponent implements OnInit{
+  
+  constructor(private contentfulService: ContentfulService){}
 
+  blogPosts$: Observable<any> | undefined;
+
+  ngOnInit(): void {
+    this.blogPosts$ = this.contentfulService.getAllEntries();      
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { ContentfulService } from 'src/app/services/contentful.service';
+import { Observable } from 'rxjs';
 
 var today = new Date();
 
@@ -9,7 +11,15 @@ var today = new Date();
   styleUrls: ['./home.component.css']
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  constructor(private contentfulService: ContentfulService){}
+
+  blogPosts$: Observable<any> | undefined;
+
+  ngOnInit(): void {
+    this.blogPosts$ = this.contentfulService.getAllEntries();      
+  }
  
   // date : any = today
 
